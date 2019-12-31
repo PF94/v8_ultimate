abbf3493d2b3c3cd37f9e9c71a241617bcb718f1
PF94 on Vista <pf94@cloudsoftware.cf>
2019-12-30 19:41:22 -0500

    Add Inverted Ramps

---
+++ Add-Ons/Bricks/Brick_InvRamps.cs
@@ -0,0 +1,19 @@
+//2x2 Inverted Corner Ramps - by Space Guy
+
+datablock fxDTSBrickData (brickRampInv45data){
+	brickFile = "base/data/bricks/ramps/2x2InvCorner.blb";
+	category = "Ramps";
+	subCategory = "45 Degree";
+	uiName = "45° Inv Ramp Corner";
+	iconName = "base/client/ui/brickIcons/45 Inv Ramp";
+	collisionshapename = "base/data/shapes/bricks/inv2x2ramp.dts";
+};
+
+datablock fxDTSBrickData (brickRampInvMinus45data){
+	brickFile = "base/data/bricks/ramps/2x2InvUpCorner.blb";
+	category = "Ramps";
+	subCategory = "45 Degree";
+	uiName = "-45° Inv Ramp Corner";
+	iconName = "base/client/ui/brickIcons/-45 Inv Ramp";
+	collisionshapename = "base/data/shapes/bricks/inv2x2ramp.dts";
+};
