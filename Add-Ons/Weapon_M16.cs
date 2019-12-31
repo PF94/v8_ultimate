dca1c0cf8969724a8dbed66c1c2d7ab0d2a85418
PF94 on Vista <pf94@cloudsoftware.cf>
2019-12-30 19:45:02 -0500

    Add yet another M16

---
+++ Add-Ons/Weapon_M16.cs
@@ -0,0 +1,510 @@
+//M16.cs
+
+//audio
+datablock AudioProfile(M16Shot1Sound)
+{
+   filename    = "./Sound/gunshot2.wav";
+   description = AudioClose3d;
+   pReady = true;
+};
+datablock AudioProfile(bulletHitSound)
+{
+   filename    = "./Sound/spearHit.wav";
+   description = AudioClose3d;
+   pReady = true;
+};
+
+
+//shell
+datablock DebrisData(M16ShellDebris)
+{
+	shapeFile = "./shapes/gunShell.dts";
+	lifetime = 2.0;
+	minSpinSpeed = -400.0;
+	maxSpinSpeed = 200.0;
+	elasticity = 0.5;
+	friction = 0.2;
+	numBounces = 3;
+	staticOnMaxBounce = true;
+	snapOnMaxBounce = false;
+	fade = true;
+
+	gravModifier = 2;
+};
+
+
+//muzzle flash effects
+datablock ParticleData(M16FlashParticle)
+{
+	dragCoefficient      = 3;
+	gravityCoefficient   = -0.5;
+	inheritedVelFactor   = 0.2;
+	constantAcceleration = 0.0;
+	lifetimeMS           = 25;
+	lifetimeVarianceMS   = 15;
+	textureName          = "base/data/particles/cloud";
+	spinSpeed		= 10.0;
+	spinRandomMin		= -500.0;
+	spinRandomMax		= 500.0;
+	colors[0]     = "0.9 0.9 0.0 0.9";
+	colors[1]     = "0.9 0.5 0.0 0.0";
+	sizes[0]      = 0.5;
+	sizes[1]      = 1.0;
+
+	useInvAlpha = false;
+};
+datablock ParticleEmitterData(M16FlashEmitter)
+{
+   ejectionPeriodMS = 3;
+   periodVarianceMS = 0;
+   ejectionVelocity = 1.0;
+   velocityVariance = 1.0;
+   ejectionOffset   = 0.0;
+   thetaMin         = 0;
+   thetaMax         = 90;
+   phiReferenceVel  = 0;
+   phiVariance      = 360;
+   overrideAdvance = false;
+   particles = "M16FlashParticle";
+};
+
+datablock ParticleData(M16SmokeParticle)
+{
+	dragCoefficient      = 3;
+	gravityCoefficient   = -0.5;
+	inheritedVelFactor   = 0.2;
+	constantAcceleration = 0.0;
+	lifetimeMS           = 525;
+	lifetimeVarianceMS   = 55;
+	textureName          = "./shapes/cloudy";
+	spinSpeed		= 10.0;
+	spinRandomMin		= -500.0;
+	spinRandomMax		= 500.0;
+	colors[0]     = "0.5 0.5 0.5 0.9";
+	colors[1]     = "0.5 0.5 0.5 0.0";
+	sizes[0]      = 0.15;
+	sizes[1]      = 0.15;
+
+	useInvAlpha = false;
+};
+datablock ParticleEmitterData(M16SmokeEmitter)
+{
+   ejectionPeriodMS = 3;
+   periodVarianceMS = 0;
+   ejectionVelocity = 1.0;
+   velocityVariance = 1.0;
+   ejectionOffset   = 0.0;
+   thetaMin         = 0;
+   thetaMax         = 90;
+   phiReferenceVel  = 0;
+   phiVariance      = 360;
+   overrideAdvance = false;
+   particles = "M16SmokeParticle";
+};
+
+
+//bullet trail effects
+datablock ParticleData(bulletTrailParticle)
+{
+	dragCoefficient      = 3;
+	gravityCoefficient   = -0.0;
+	inheritedVelFactor   = 1.0;
+	constantAcceleration = 0.0;
+	lifetimeMS           = 525;
+	lifetimeVarianceMS   = 55;
+	textureName          = "base/data/particles/thinRing";
+	spinSpeed		= 10.0;
+	spinRandomMin		= -500.0;
+	spinRandomMax		= 500.0;
+	colors[0]     = "0.3 0.3 0.9 0.4";
+	colors[1]     = "0.5 0.5 0.5 0.0";
+	sizes[0]      = 0.15;
+	sizes[1]      = 0.25;
+
+	useInvAlpha = false;
+};
+datablock ParticleEmitterData(bulletTrailEmitter)
+{
+   ejectionPeriodMS = 3;
+   periodVarianceMS = 0;
+   ejectionVelocity = 0.0;
+   velocityVariance = 0.0;
+   ejectionOffset   = 0.0;
+   thetaMin         = 0;
+   thetaMax         = 90;
+   phiReferenceVel  = 0;
+   phiVariance      = 360;
+   overrideAdvance = false;
+   particles = "bulletTrailParticle";
+};
+
+
+datablock ParticleData(M16ExplosionParticle)
+{
+	dragCoefficient      = 8;
+	gravityCoefficient   = -0.5;
+	inheritedVelFactor   = 0.2;
+	constantAcceleration = 0.0;
+	lifetimeMS           = 700;
+	lifetimeVarianceMS   = 400;
+	textureName          = "./Shapes/snow";
+	spinSpeed		= 10.0;
+	spinRandomMin		= -50.0;
+	spinRandomMax		= 50.0;
+	colors[0]     = "0.9 0.9 0.6 0.9";
+	colors[1]     = "0.9 0.5 0.6 0.0";
+	sizes[0]      = 0.25;
+	sizes[1]      = 1.0;
+
+	useInvAlpha = true;
+};
+datablock ParticleEmitterData(M16ExplosionEmitter)
+{
+   ejectionPeriodMS = 1;
+   periodVarianceMS = 0;
+   ejectionVelocity = 5;
+   velocityVariance = 1.0;
+   ejectionOffset   = 0.0;
+   thetaMin         = 89;
+   thetaMax         = 90;
+   phiReferenceVel  = 0;
+   phiVariance      = 360;
+   overrideAdvance = false;
+   particles = "M16ExplosionParticle";
+};
+
+
+datablock ParticleData(M16ExplosionRingParticle)
+{
+	dragCoefficient      = 8;
+	gravityCoefficient   = -0.5;
+	inheritedVelFactor   = 0.2;
+	constantAcceleration = 0.0;
+	lifetimeMS           = 300;
+	lifetimeVarianceMS   = 100;
+	textureName          = "base/data/particles/star1";
+	spinSpeed		= 10.0;
+	spinRandomMin		= -50.0;
+	spinRandomMax		= 50.0;
+	colors[0]     = "1 1 0.0 0.9";
+	colors[1]     = "0.9 0.0 0.0 0.0";
+	sizes[0]      = 0.2;
+	sizes[1]      = 0.2;
+
+	useInvAlpha = false;
+};
+datablock ParticleEmitterData(M16ExplosionRingEmitter)
+{
+	lifeTimeMS = 50;
+
+   ejectionPeriodMS = 1;
+   periodVarianceMS = 0;
+   ejectionVelocity = 5;
+   velocityVariance = 0.0;
+   ejectionOffset   = 0.0;
+   thetaMin         = 89;
+   thetaMax         = 90;
+   phiReferenceVel  = 0;
+   phiVariance      = 360;
+   overrideAdvance = false;
+   particles = "M16ExplosionRingParticle";
+};
+
+datablock ExplosionData(M16Explosion)
+{
+   //explosionShape = "";
+	soundProfile = bulletHitSound;
+
+   lifeTimeMS = 150;
+
+   particleEmitter = M16ExplosionEmitter;
+   particleDensity = 10;
+   particleRadius = 0.2;
+
+   emitter[0] = M16ExplosionRingEmitter;
+
+   faceViewer     = true;
+   explosionScale = "1 1 1";
+
+   shakeCamera = false;
+   camShakeFreq = "10.0 11.0 10.0";
+   camShakeAmp = "1.0 1.0 1.0";
+   camShakeDuration = 0.5;
+   camShakeRadius = 10.0;
+
+   // Dynamic light
+   lightStartRadius = 0;
+   lightEndRadius = 2;
+   lightStartColor = "0.3 0.6 0.7";
+   lightEndColor = "0 0 0";
+};
+
+
+AddDamageType("M16",   '<bitmap:add-ons/ci/M16> %1',    '%2 <bitmap:add-ons/ci/M16> %1',0.5,1);
+datablock ProjectileData(M16Projectile)
+{
+   projectileShapeName = "./shapes/bullet.dts";
+   directDamage        = 45;
+   directDamageType    = $DamageType::M16;
+   radiusDamageType    = $DamageType::M16;
+
+   brickExplosionRadius = 1;
+   brickExplosionImpact = true;          //destroy a brick if we hit it directly?
+   brickExplosionForce  = 1;
+   brickExplosionMaxVolume = 60;          //max volume of bricks that we can destroy
+   brickExplosionMaxVolumeFloating = 999;  //max volume of bricks that we can destroy if they aren't connected to the ground
+
+   impactImpulse	     = 200;
+   verticalImpulse	  = 200;
+   explosion           = M16Explosion;
+   particleEmitter     = bulletTrailEmitter;
+
+   muzzleVelocity      = 700;
+   velInheritFactor    = 1;
+
+   armingDelay         = 00;
+   lifetime            = 4000;
+   fadeDelay           = 3500;
+   bounceElasticity    = 0.5;
+   bounceFriction      = 0.20;
+   isBallistic         = false;
+   gravityMod = 0.0;
+
+   hasLight    = false;
+   lightRadius = 3.0;
+   lightColor  = "0 0 0.5";
+};
+
+//////////
+// item //
+//////////
+datablock ItemData(M16Item)
+{
+	category = "Weapon";  // Mission editor category
+	className = "Weapon"; // For inventory system
+
+	 // Basic Item Properties
+	shapeFile = "./shapes/M16.dts";
+	rotate = false;
+	mass = 1;
+	density = 0.2;
+	elasticity = 0.2;
+	friction = 0.6;
+	emap = true;
+
+	//gui stuff
+	uiName = "M16";
+	iconName = "./ItemIcons/M16";
+	doColorShift = false;
+	colorShiftColor = "0.25 0.25 0.25 1.000";
+
+	 // Dynamic properties defined by the scripts
+	image = M16Image;
+	canDrop = true;
+};
+
+//function M16Item::onUse(%this, %player, %InvPosition)
+//{
+//	//check for quiver 
+//	//if you dont have it, regular bow
+//	//if you do, super bow
+//
+//	%client = %player.client;
+//
+//	%mountPoint = %this.image.mountPoint;
+//	%mountedImage = %player.getMountedImage(%mountPoint); 
+//
+//
+//	if(%mountedImage)
+//	{
+//		if(%mountedImage == M16Image.getId() || %mountedImage == superbowImage.getId())
+//		{
+//			//some kind of bow mounted so, unmount it
+//			%player.unMountImage(%mountPoint);
+//			messageClient(%client, 'MsgHilightInv', '', -1);
+//			%player.currWeaponSlot = -1;
+//		}
+//		else
+//		{
+//			//something other than bow mounted, so do bow selection and mount
+//			if(%player.getMountedImage($BackSlot))
+//			{
+//				if(%player.getMountedImage($BackSlot) == quiverImage.getId())
+//				{
+//					%player.mountimage(superBowImage, $RightHandSlot, 1, %skin);
+//					messageClient(%client, 'MsgHilightInv', '', %InvPosition);
+//					%player.currWeaponSlot = %invPosition;
+//				}
+//				else
+//				{
+//					%player.mountimage(bowImage, $RightHandSlot, 1, %skin);
+//					messageClient(%client, 'MsgHilightInv', '', %InvPosition);
+//					%player.currWeaponSlot = %invPosition;
+//				}
+//			}
+//			else
+//			{
+//				%player.mountimage(bowImage, $RightHandSlot, 1, %skin);
+//				messageClient(%client, 'MsgHilightInv', '', %InvPosition);
+//				%player.currWeaponSlot = %invPosition;
+//			}
+//		}
+//		
+//	}
+//	else
+//	{
+//		//nothing mounted so do bow selection and mount
+//		//something other than bow mounted, so do bow selection and mount
+//		if(%player.getMountedImage($BackSlot))
+//		{
+//			if(%player.getMountedImage($BackSlot) == quiverImage.getId())
+//			{
+//				%player.mountimage(superBowImage, $RightHandSlot, 1, %skin);
+//				messageClient(%client, 'MsgHilightInv', '', %InvPosition);
+//				%player.currWeaponSlot = %invPosition;
+//			}
+//			else
+//			{
+//				%player.mountimage(bowImage, $RightHandSlot, 1, %skin);
+//				messageClient(%client, 'MsgHilightInv', '', %InvPosition);
+//				%player.currWeaponSlot = %invPosition;
+//			}
+//		}
+//		else
+//		{
+//			%player.mountimage(bowImage, $RightHandSlot, 1, %skin);
+//			messageClient(%client, 'MsgHilightInv', '', %InvPosition);
+//			%player.currWeaponSlot = %invPosition;
+//		}
+//	}
+//}
+
+////////////////
+//weapon image//
+////////////////
+datablock ShapeBaseImageData(M16Image)
+{
+   // Basic Item properties
+   shapeFile = "./shapes/M16.dts";
+   emap = true;
+
+   // Specify mount point & offset for 3rd person, and eye offset
+   // for first person rendering.
+   mountPoint = 0;
+   offset = "0 0 0";
+   eyeOffset = 0; //"0.7 1.2 -0.5";
+   rotation = eulerToMatrix( "0 0 0" );
+
+   // When firing from a point offset from the eye, muzzle correction
+   // will adjust the muzzle vector to point to the eye LOS point.
+   // Since this weapon doesn't actually fire from the muzzle point,
+   // we need to turn this off.  
+   correctMuzzleVector = true;
+
+   // Add the WeaponImage namespace as a parent, WeaponImage namespace
+   // provides some hooks into the inventory system.
+   className = "WeaponImage";
+
+   // Projectile && Ammo.
+   item = M16Item;
+   ammo = " ";
+   projectile = M16Projectile;
+   projectileType = Projectile;
+
+	casing = M16ShellDebris;
+	shellExitDir        = "1.0 -1.3 1.0";
+	shellExitOffset     = "0 0 0";
+	shellExitVariance   = 15.0;	
+	shellVelocity       = 7.0;
+
+   //melee particles shoot from eye node for consistancy
+   melee = false;
+   //raise your arm up or not
+   armReady = true;
+
+   doColorShift = false;
+   colorShiftColor = M16Item.colorShiftColor;//"0.000 0.000 0.000";
+
+   //casing = " ";
+
+   // Images have a state system which controls how the animations
+   // are run, which sounds are played, script callbacks, etc. This
+   // state system is downloaded to the client so that clients can
+   // predict state changes and animate accordingly.  The following
+   // system supports basic ready->fire->Ready transitions as
+   // well as a no-ammo->dryfire idle state.
+
+   // Initial start up state
+	stateName[0]                     = "Activate";
+	stateTimeoutValue[0]             = 0.05;
+	stateTransitionOnTimeout[0]       = "Ready";
+	stateSound[0]					= weaponSwitchSound;
+
+	stateName[1]                     = "Ready";
+	stateTransitionOnTriggerDown[1]  = "Fire";
+	stateAllowImageChange[1]         = true;
+	stateSequence[1]	= "Ready";
+
+	stateName[2]                    = "Fire";
+	stateTransitionOnTimeout[2]     = "Smoke";
+	stateTimeoutValue[2]            = 0.05;
+	stateFire[2]                    = true;
+	stateAllowImageChange[2]        = false;
+	stateSequence[2]                = "Fire";
+	stateScript[2]                  = "onFire";
+	stateWaitForTimeout[2]			= true;
+	stateEmitter[2]					= M16FlashEmitter;
+	stateEmitterTime[2]				= 0.05;
+	stateEmitterNode[2]				= "muzzleNode";
+	stateSound[2]					= M16Shot1Sound;
+	stateEjectShell[2]       = true;
+
+	stateName[3] = "Smoke";
+	stateEmitter[3]					= M16SmokeEmitter;
+	stateEmitterTime[3]				= 0.05;
+	stateEmitterNode[3]				= "muzzleNode";
+	stateTimeoutValue[3]            = 0.008;
+	stateTransitionOnTimeout[3]     = "Ready";
+
+	stateName[4]			= "Ready";
+	stateSequence[4]                = "Ready";
+	stateTransitionOnTriggerUp[4]     = "Ready";
+	stateSequence[4]	= "Ready";
+};
+
+
+function M16Image::onFire(%this,%obj,%slot)
+{
+	%obj.setVelocity(VectorAdd(%obj.getVelocity(),VectorScale(%obj.client.player.getEyeVector(),"-1.5")));
+	
+
+	%projectile = %this.projectile;
+	%spread = 0.0020;
+	%shellcount = 1;
+
+	for(%shell=0; %shell<%shellcount; %shell++)
+	{
+		%vector = %obj.getMuzzleVector(%slot);
+		%objectVelocity = %obj.getVelocity();
+		%vector1 = VectorScale(%vector, %projectile.muzzleVelocity);
+		%vector2 = VectorScale(%objectVelocity, %projectile.velInheritFactor);
+		%velocity = VectorAdd(%vector1,%vector2);
+		%x = (getRandom() - 0.5) * 10 * 3.1415926 * %spread;
+		%y = (getRandom() - 0.5) * 10 * 3.1415926 * %spread;
+		%z = (getRandom() - 0.5) * 10 * 3.1415926 * %spread;
+		%mat = MatrixCreateFromEuler(%x @ " " @ %y @ " " @ %z);
+		%velocity = MatrixMulVector(%mat, %velocity);
+
+		%p = new (%this.projectileType)()
+		{
+			dataBlock = %projectile;
+			initialVelocity = %velocity;
+			initialPosition = %obj.getMuzzlePoint(%slot);
+			sourceObject = %obj;
+			sourceSlot = %slot;
+			client = %obj.client;
+		};
+		MissionCleanup.add(%p);
+	}
+	return %p;
+}
