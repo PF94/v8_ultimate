fb8a7fa6a5b9def59be4162f1d7144163d02b554
PF94 on Vista <pf94@cloudsoftware.cf>
2019-12-30 20:16:43 -0500

    Add more brick print addons

---
+++ Add-Ons/Bricks/Brick_Prints.cs
@@ -0,0 +1,25 @@
+//Extra Print Bricks plus No Lines 1x1 - by Space Guy
+
+datablock fxDTSBrickData (brick1x1FBPrintData : brick1x1FPrintData){
+	brickFile = "./1x1FBPrint.blb";
+	uiName = "Bottom Print 1x1F";
+	iconName = "./brickIcons/1x1FBPrint";
+};
+
+datablock fxDTSBrickData (brick2x2FBPrintData : brick2x2FPrintData){
+	brickFile = "./2x2FBPrint.blb";
+	uiName = "Bottom Print 2x2F";
+	iconName = "./brickIcons/2x2FBPrint";
+};
+
+datablock fxDTSBrickData (brick1x14PrintData : brick1x1PrintData){
+	brickFile = "./1x1-4Print.blb";
+	uiName = "4-Sided Print 1x1";
+	iconName = "./brickIcons/1x14Print";
+};
+
+datablock fxDTSBrickData (brick1x1NoLineData : brick1x1PrintData){
+	brickFile = "./1x1-NoLines.blb";
+	uiName = "No Lines 1x1";
+	iconName = "./brickIcons/1x1NoLines";
+};
